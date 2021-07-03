97ef5c9d04eb51c075ede95fd90b9783b327c615
valryne378 <85669285+valryne378@users.noreply.github.com>
2021-07-03 21:20:11 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,19 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> len=int(input("enter the length")
+	4
+	
+SyntaxError: invalid syntax
+>>> len=int(input("enter the len")
+	s="abcderfgjd"
+	
+SyntaxError: invalid syntax
+>>> import random
+>>> len=int(input("len"))
+len 4
+>>> s="abcdefghijklmnopqrs"
+>>> p="".join(random.sample(s,len))
+>>> print(p)
+rcsk
+>>> 
